1dd931c1539ecc74a01845c6d00d3030fc3d0ef7
Lam Thong <max.phan@endava.com>
2026-01-10 20:38:13 +0700

    updated config for Playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-  testDir: './tests',
-  reporter: [['html', { open: 'never' }]],
-  retries: process.env.CI ? 2 : 0,
   use: {
-    trace: 'on-first-retry',
-    screenshot: 'only-on-failure',
-    video: 'retain-on-failure',
-    baseURL: process.env.BASE_URL || 'https://example.com',
+    baseURL: process.env.BASE_URL || 'http://localhost:8080',
+  },
+
+  webServer: {
+    command: 'npm run serve',
+    port: 8080,
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI,
   },
 });
